8b54d2c73d96cc289e74b16bc7d5bc5612620067
NaniTadala <92290291+NaniTadala@users.noreply.github.com>
2021-10-20 12:33:15 +0530

    Add files via upload

---
+++ Data Types.c
@@ -0,0 +1,20 @@
+//C Program to print various data types
+#include<stdio.h>
+void main()
+{
+	//Variable Declaration
+	int age;
+	double cgpa;
+	char initial;
+	
+	//initialization
+	age = 19;
+	cgpa = 8.1;
+	initial = 'T';
+	
+	//Output
+	printf("Initial of the Student is: %c\n",initial);
+	printf("Age of the Student is: %d\n",age);
+	printf("CGPA of the Student is: %lf\n",cgpa);
+
+}
