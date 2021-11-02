7ad212c393f49c1f75e8bcc7a6018c6f118f932b
NaniTadala <92290291+NaniTadala@users.noreply.github.com>
2021-11-02 12:24:04 +0530

    Add files via upload

---
+++ Sum of natural numbers.c
@@ -0,0 +1,16 @@
+//Sum of First n natural numbers
+#include<stdio.h>
+void main ()
+{
+	//Varible Declaration
+	int n, s;
+	
+	//Initialization
+	n=5;
+	
+	//Calculation
+	s= n*(n+1)/2;
+	
+	//Output
+	printf("Sum of first %d natural numbers is %d\n",n, s);
+}
