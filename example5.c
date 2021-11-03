d4af58f87dbbb2278cc9ca45a31eb693ef0ea73e
NaniTadala <92290291+NaniTadala@users.noreply.github.com>
2021-11-03 12:22:37 +0530

    Add files via upload

---
+++ example5.c
@@ -0,0 +1,10 @@
+//Write a C Program to find out the area of a circle when radius is given
+#include<stdio.h>
+ int main() 
+ {
+    float r, a;
+    r= 15;
+    a = 3.14*r*r; 
+    
+    printf("the area of the circle is %f\n",a);
+}
